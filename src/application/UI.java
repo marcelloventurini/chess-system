cf067ab5ba7e39162776bc9d671f46a06e1a163b
Marcello Venturini <marcellofventurini@gmail.com>
2022-10-14 11:48:42 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,7 +32,7 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	public static void clearScreen() {
-		System.out.println("\033[H\033[2J");
+		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
 
@@ -57,10 +57,17 @@ public class UI {
 		System.out.println();
 
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player " + chessMatch.getCurrentPlayer());
 
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player " + chessMatch.getCurrentPlayer());
+
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
